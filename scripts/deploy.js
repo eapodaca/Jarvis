ef7527101688fd7f4ccda9a068bf18f7d2e73255
Ethan Apodaca <papodaca@gmail.com>
2018-04-14 10:50:24 -0700

    Add redis brain

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,18 @@
-module.exports = function(robot) {
+module.exports = (robot) => {
   robot.router.post("/circle-ci", (req, res) => {
-    const data = req.body;
-    robot.send({room: "town-square"}, JSON.stringify(data));
+    const data = req.body.payload;
+    message = "";
+    if(!data.failed) {
+      message = `:ok_man:  I've been deployed successfully by ${data.user.login}`;
+    } else {
+      message = `:interrobang:  My deploy failed. Shame on you ${data.user.login}.`;
+    }
+    message = `${message}\nChanges [here](${data.compare}).`
+    robot.messageRoom("duxb9ptk8pr8mcwnhounggg51y", message);
     res.send("OK");
+  });
+  robot.router.get("/circle-ci-users", (req, res) => {
+    const data = req.body;
+    res.send(JSON.stringify(robot.brain.users()));
   });
 };
