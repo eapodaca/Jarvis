808ba9dc7b9228a7b5d4dffbd433c1dd8f1d86b5
anotherdirtbag <27506781+anotherdirtbag@users.noreply.github.com>
2018-04-25 11:11:27 -0700

    Still not sure why this debbugging isn't working

--- scripts/demolition-man.js
+++ scripts/demolition-man.js
@@ -320,7 +320,7 @@ const insults = [
 
 module.exports = function(robot) {
 	robot.respond(/swear\s+(.*)?\s*/i, function(msg) {
-		var preparsed_msg = msg.replace(/[^\w\s"]/g, '').replace(/\s+/g,' ');
+		var preparsed_msg = msg.replace(/[^\w\s"]/g, ''); //.replace(/\s+/g,' ');
 		var regex_result = preparsed_msg.match(new RegExp(`(?:(?:^|[^a-z"])(${insults.join('|')})(?:[a-z]?ed|es|ing|s)?(?:[^a-z"]|$))(?=(?:[^"]*"[^"]*")*[^"]*$)`, 'i'));
 		return msg.send('msg:' + msg + '; preparsed_msg:' + preparsed_msg + '; matched:' + (regex_result? 'true' : 'false') + '; firstmatch:' + msg.match[1]);
 		//if ( regex_result ) {
