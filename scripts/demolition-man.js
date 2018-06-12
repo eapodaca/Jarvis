0f734c5cd404042b4022a2f9d075cb2a4baa1470
anotherdirtbag <27506781+anotherdirtbag@users.noreply.github.com>
2018-06-12 13:15:45 -0700

    now I'm just guessing

--- scripts/demolition-man.js
+++ scripts/demolition-man.js
@@ -333,7 +333,7 @@ const fineableoffense = [
 
 module.exports = function(robot) {
   const regex = new RegExp(`(?:(?:^|[^a-z"])(${fineableoffense.join('|')})(?:[a-z]?ed|es|ing|s)?(?:[^a-z"]|$))(?=(?:[^"]*"[^"]*")*[^"]*$)`, 'i');
-  robot.hear(regex, msg => msg.send(`${robot.brain.userForId(msg.envelope.user.id)} has been fined one credit for a violation of the verbal morality statute.`));
+  robot.hear(regex, msg => msg.send(`${robot.brain.userForId(msg.envelope.user.id).name} has been fined one credit for a violation of the verbal morality statute.`));
   
   return robot.respond(/insult/i, msg => msg.send(`${insults[Math.floor(Math.random() * insults.length)].replace(/[\\\+]+/g, '')}`));
 };
